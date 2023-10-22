9ec90aadbe1c60a6c61f77aad272d2d213e5a561
EsmailRafehi123 <148717024+EsmailRafehi123@users.noreply.github.com>
2023-10-22 21:53:14 +0200

    Add files via upload

---
+++ GA/Java script fil.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const searchForm = document.querySelector("searchForm"); // Om du vill välja det första formuläret på sidan
+    searchForm.addEventListener("submit", function(event) {
+      event.preventDefault(); // Förhindra standardformulärskickning
+  
+      const searchQuery = document.getElementById("searchQuery").value;
+      const searchUrl = 'search.html?q=${encodeURIComponent(searchQuery)}';
+  
+      // Omdirigera användaren till sökresultatsidan
+      window.location.href = searchUrl;
+    });
+  });
+  
