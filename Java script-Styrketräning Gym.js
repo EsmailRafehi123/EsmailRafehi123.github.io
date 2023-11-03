6e23d020b0731c4698a5f9f06b096ddba589ac8b
EsmailRafehi123 <148717024+EsmailRafehi123@users.noreply.github.com>
2023-11-03 09:35:47 +0100

    Add files via upload

---
+++ Java script-Styrketräning Gym.js
@@ -0,0 +1,12 @@
+/*
+function updateClock() {
+    var now = new Date();
+    var time = now.toLocaleTimeString();
+    var date = now.toLocaleDateString();
+    document.getElementById("clock").textContent = time;
+    document.getElementById("date").textContent = date;
+  }
+  setInterval(updateClock, 1000);
+  /** */
+
+  
